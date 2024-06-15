b20749b6887414aa734d59b10a22a8742f969818
Evan Yan <103996156+evanyan13@users.noreply.github.com>
2024-06-15 18:36:35 +0800

    Add Hero, NavBar and Footer to home page

--- frontend/components/CustomButton.tsx
+++ frontend/components/CustomButton.tsx
@@ -5,11 +5,14 @@ import Image from 'next/image'
 import { CustomButtonProps } from '@/types'
 import { Tienne } from 'next/font/google'
 
-const CustomButton = ({ title, containerStyles, handleClick }: CustomButtonProps) => {
+const CustomButton = ({
+  title, containerStyles,
+  handleClick, btnType,
+ }: CustomButtonProps) => {
   return (
     <button
         disabled={false}
-        type={"button"}
+        type={btnType || "button"}
         className={`custom-btn ${ containerStyles }`}
         onClick={handleClick}
     >
--- frontend/components/Hero.tsx
+++ frontend/components/Hero.tsx
@@ -11,26 +11,29 @@ const Hero = () => {
 
   return (
     <div className='hero'>
-        <div className='flex-1 pt-36 padding-x'>
-            <h1 className='hero__title'>
-                Find or Rent a Car Today
-            </h1>
-            <p>
-                Streamline your car rental experience
-            </p>
-            <CustomButton 
-              title='Explore Cars'
-              containerStyles='bg-primary-blue text-white rounded-full mt-10'
-              handleClick={ handleScroll }
-            />
-            <div className='hero__image-container'>
-              <div className='hero__image'>
-                <Image src='/hero.png' alt='hero' fill
-                  className='object-contain'/>
-                  <div className='hero__image-overlay'/>
-              </div>
-            </div>
+
+      <div className='flex-1 pt-36 padding-x'>
+          <h1 className='hero__title'>
+              Find or Rent a Car Today
+          </h1>
+          <p>
+              Streamline your car rental experience
+          </p>
+          <CustomButton 
+            title='Explore Cars'
+            containerStyles='bg-primary-blue text-white rounded-full mt-10'
+            handleClick={ handleScroll }
+          />
+      </div>
+
+      <div className='hero__image-container'>
+        <div className='hero__image'>
+          <Image src='/hero.png' alt='hero' fill
+            className='object-contain'/>
         </div>
+        <div className='hero__image-overlay'/>
+      </div>
+      
     </div>
   )
 }
