22c59d79abea1c3b89eb4b2a5d1884fa32a9a643
Evan Yan <103996156+evanyan13@users.noreply.github.com>
2024-07-08 11:05:27 +0800

    Add LP Chart

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,14 +1,27 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import { Button } from '@/components/ui/button';
+import { Calendar } from 'lucide-react';
+import { getToday } from '@/lib/utils';
+import { LabourProgressChart } from '@/components/LabourProgressChart';
 
 export default function Home() {
+  const today = getToday();
+
   return (
-   <main>
-      <Link href="/dashboard">
-        <Button className="primary-button">
-          Home
-        </Button>
-      </Link>
-   </main>
+    <main className="h-full">
+      <div className="flex-1 space-y-4 p-4 pt-6 md:p-8">
+        <div id="top-bar" className="flex w-full justify-between">
+          <h2 className="text-3xl font-bold tracking-tight">
+              Hi, Welcome back 👋
+          </h2>
+          <Button variant="outline" className="gap-2 text-gray-700">
+            <Calendar size={20} />
+            { today }
+          </Button>
+        </div>
+        <div id="labour-progression-chart">
+          <LabourProgressChart />
+        </div>
+      </div>
+    </main>
   );
 }
--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -4,3 +4,8 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function getToday() {
+  const today = new Date()
+  return today.toDateString()
+}
